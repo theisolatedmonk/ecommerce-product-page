c3c5ee24bab42a391d0a329cdbccd6b69a855941
theisolatedmonk <96rahulseth@gmail.com>
2023-04-27 16:26:35 +0530

    initial mobile design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,85 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import cart from "@/images/icon-cart.svg";
+import close from "@/images/icon-close.svg";
+import delet from "@/images/icon-delete.svg";
+import menu from "@/images/icon-menu.svg";
+import minus from "@/images/icon-minus.svg";
+import next from "@/images/icon-next.svg";
+import plus from "@/images/icon-plus.svg";
+import previous from "@/images/icon-previous.svg";
+import avatar from "@/images/image-avatar.png";
+import product1Thumb from "@/images/image-product-1-thumbnail.jpg";
+import product1 from "@/images/image-product-1.jpg";
+import product2Thumb from "@/images/image-product-2-thumbnail.jpg";
+import product2 from "@/images/image-product-2.jpg";
+import product3Thumb from "@/images/image-product-3-thumbnail.jpg";
+import product3 from "@/images/image-product-3.jpg";
+import product4Thumb from "@/images/image-product-4-thumbnail.jpg";
+import product4 from "@/images/image-product-4.jpg";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=" flex  justify-center items-center bg-black min-h-screen-h-screen">
+      <div className="w-64 text-black h-full bg-white flex flex-col gap-2">
+        <div className="header flex items-center gap-28 w-full p-2">
+          <div className="flex gap-2  items-center">
+            <div className="h-3 w-3 flex justify-center items-center">
+            <Image className=" " src={menu} alt="" />
+            </div>
+            <p>sneakers</p>
+          </div>
+          <div className="flex gap-2 h-4 w-4">
+            <Image className="" src={cart} alt="" />
+            <Image className="" src={avatar} alt="" />
+          </div>
+        </div>
+
+        <div className="imgbox relative">
+          <Image className="" src={product1} alt="" />
+          <div className="flex gap-[176px] px-2 w-full items-center  absolute mt-[-140px]">
+            <button className=" bg-white rounded-full p-2 h-8 w-8 flex items-center">
+              <Image className="absolute " src={previous} alt="" />
+            </button>
+            <button className=" bg-white rounded-full p-2 flex items-center h-8 w-8  ">
+              <Image className="absolute" src={next} alt="" />
+            </button>
+          </div>
+        </div>
+
+        <div className="paracontainer p-2">
+          <p>SNEAKER COMPANY</p>
+          <p>Fall Limited Edition Sneakers</p>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they'll withstand everything
+            the weather can offer.
+          </p>
+        </div>
+
+        <div className="price flex justify-between p-2">
+          <div className="flex gap-2">
+            <p>$125.00</p>
+            <p className="bg-orange-500 rounded-md p-1">50%</p>
+          </div>
+          <div>
+            <p className="line-through">$250.00</p>
+          </div>
+        </div>
+
+        <div className="p-4"><div className="itemcount flex justify-between items-center rounded-lg p-2 bg-slate-100">
+          <button><Image className=" " src={minus} alt="" /></button>
+          <p>0</p>
+          <button className="flex justify-center  items-center"><Image className="  " src={plus} alt="" /></button>
+        </div></div>
+        
+        <div className="p-3 text-white">
+        <button className="flex justify-center items-center p-2 rounded-lg bg-orange-500 w-full gap-3">
+        <Image className="" src={cart} alt="" />
+          <p className="">Add to cart</p>
+        </button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
